84fecfd12cef4125277660e570f38dce35a30637
getxt <104286270+TechKeep@users.noreply.github.com>
2022-04-24 18:14:14 -0400

    Create archun.sh

---
+++ archun.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# archun by github.com/TechKeep
+
+mainMenu() {
+  echo "- - ArchUn: Part 1 - -"
+  echo "ArchUn is an automatic ArchLinux install script."
+  echo "You can either start the script with default settings, or use some custom values."
+  echo "!! WARNING !! - This will ERASE EVERYTHING on the device you're using it from."
+  PS3='Which process do you want? (ENTER to confirm): '
+  options=("Automatic" "Custom" "Continue to Part 2" "Quit")
+  select opt in "${options[@]}"
+  do
+      case $opt in
+          "Automatic")
+              startAutomaticInstProcess
+              ;;
+          "Custom")
+              startCustomInstProcess
+              ;;
+          "Continue to Part 2")
+              continueToPart2
+              ;;
+          "Quit")
+              exit
+              ;;
+          *) echo "invalid option $REPLY";;
+      esac
+  done
+}
+
+clear
+mainMenu
