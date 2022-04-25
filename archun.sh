5f7e94d2d5a1aac15a5134a2b9c7565b438dc3c5
getxt <104286270+TechKeep@users.noreply.github.com>
2022-04-24 20:35:32 -0400

    Update archun.sh

--- archun.sh
+++ archun.sh
@@ -1,12 +1,70 @@
 #!/bin/bash
 # archun by github.com/TechKeep
 
+######################################
+#### These values are for testing.####
+#### They will be definable later ####
+#### and a lot of these comments  ####
+#### will be deleted.             ####
+######################################
+
+USERBIOSTYPE="bios" # "bios" or "uefi"
+
+# The default disk (find out with "fdisk -l")
+DEFAULTDISK="/dev/sda"
+
+# I'll calculate sizes automatically
+# once I'm done and everything works
+if $USERBIOSTYPE="bios"
+then
+	createThePartitions() {
+		# Swap partition size. # Total size: 4096MiB
+		SWAPPARTSTART="16MiB" # Starts at position 16MiB
+		SWAPPARTEND="4112MiB" # Ends at position 44112MiB
+
+		# Root partition size. # Total size: rest of disk
+		ROOTPARTSTART="4113MiB" # Starts at position 4113MiB
+		ROOTPARTEND="100%" # Ends at the end of the disk
+
+		# Create the partitions with provided options
+		sudo parted $DEFAULTDISK --script mklabel mbr # MBR for BIOS
+		sudo parted $DEFAULTDISK --script mkpart primary linux-swap $SWAPPARTSTART $SWAPPARTEND
+		sudo parted $DEFAULTDISK --script mkpart primary ext4 $ROOTPARTSTART $ROOTPARTEND
+	}
+
+else
+	createThePartitions() {
+		# Boot partition size. # Total size: 300MiB
+		BOOTPARTSTART="16MiB" # Starts at position 16MiB
+		BOOTPARTEND="316MiB" # Ends at position 316MiB
+
+		# Swap partition size. # Total size: 4096MiB
+		SWAPPARTSTART="317MiB" # Starts at position 317MiB
+		SWAPPARTEND="4413MiB" # Ends at position 4413MiB
+
+		# Root partition size. # Total size: rest of disk
+		ROOTPARTSTART="4414MiB" # Starts at position 4414MiB
+		ROOTPARTEND="100%" # Ends at the end of the disk
+
+		# Create the partitions with provided options
+		sudo parted $DEFAULTDISK --script mklabel gpt # GPT for UEFI
+		sudo parted $DEFAULTDISK --script mkpart primary fat32 $BOOTPARTSTART $BOOTPARTEND
+		sudo parted $DEFAULTDISK --script mkpart primary linux-swap $SWAPPARTSTART $SWAPPARTEND
+		sudo parted $DEFAULTDISK --script mkpart primary ext4 $ROOTPARTSTART $ROOTPARTEND
+	}
+fi
+
+######################################
+
 startCustomInstProcess() {
 	echo "You have selected the AUTOMATIC process."
 	echo "!! WARNING !! - EVERYTHING will be ERASED on this device."
 	read -p "If you want to proceed, press ENTER. If not, type CTRL+C."
 	read -p "Last warning. Are you sure? Press ENTER to proceed."
-
+	echo "Setting date and time"
+	timedatectl set-ntp true
+	timedatectl status
+	createThePartitions
 }
 
 
