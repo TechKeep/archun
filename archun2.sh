11ebbb426c9da86b64f075e4a0374ee18ca28c1a
getxt <104286270+TechKeep@users.noreply.github.com>
2022-04-25 00:48:04 -0400

    Update archun2.sh

--- archun2.sh
+++ archun2.sh
@@ -27,3 +27,27 @@ echo $THEHOSTNAME >> /etc/hostname
 mkinitcpio -P
 # Set a root password
 passwd
+
+# Install GRUB
+pacman -Syu grub efibootmgr
+mkdir /boot/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Choose
+PS3='Which process do you want? (ENTER to confirm): '
+options=("default" "i386-pc" "x86_64-efi")
+select opt in "${options[@]}"
+do
+  case $opt in
+  	  "default")
+          grub-install /dev/sda
+          ;;
+      "i386-pc")
+          grub-install --force --target=i386-pc --recheck $DEFAULTDISK
+          ;;
+      "x86_64-efi")
+          grub-install --target=x86_64-efi --efi-directory=/boot --recheck $DEFAULTDISK
+          ;;
+      *) echo "invalid option $REPLY";;
+  esac
+done
